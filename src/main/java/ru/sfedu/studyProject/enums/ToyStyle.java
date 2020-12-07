2f66270f7450a86c874e4b9623da84daf583846d
Rincewind-z <asyaprihodko@mail.ru>
2020-12-07 21:02:33 +0300

    added enums

--- src/main/java/ru/sfedu/studyProject/enums/ToyStyle.java
+++ src/main/java/ru/sfedu/studyProject/enums/ToyStyle.java
@@ -1,4 +1,13 @@
 package ru.sfedu.studyProject.enums;
 
+ /*
+ SOFT - Мягкая игрушка
+ SOLID - Игрушка из твёрдых материалов
+ MIXED - Игрушка в стешанной технике, т.е совмещает в себе мягкие и твёрдые материалы
+ */
+
 public enum ToyStyle {
+    SOFT,
+    SOLID,
+    MIXED
 }
