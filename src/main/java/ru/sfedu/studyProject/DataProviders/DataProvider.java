c84411c27c7f549a581d4a0bc16ca37d62f8af1f
Rincewind-z <asyaprihodko@mail.ru>
2020-12-10 19:17:48 +0300

    changed the name of addMaterial to createMaterial changed the addOutgoing and editOutgoing methods to setOutgoing in the name of variables, words have been added to indicate the class to which they refer

--- src/main/java/ru/sfedu/studyProject/DataProviders/DataProvider.java
+++ src/main/java/ru/sfedu/studyProject/DataProviders/DataProvider.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import java.util.Date;
 
 public interface DataProvider {
-    boolean addMaterial (long userId,
+    boolean createMaterial (long userId,
                          String materialName,
                          float cost,
                          Unit unit,
@@ -21,7 +21,7 @@ public interface DataProvider {
     boolean editMaterial (long userId, Material editedMaterial);
     boolean deleteMaterial (long userId, long id);
 
-    Optional<Material> getMaterial (long userId, long id);
+    Optional<Material> getMaterial (long userId, long materialId);
     List<Material> getMaterial (long userId);
 
     boolean createProject (long userId,
@@ -37,33 +37,31 @@ public interface DataProvider {
                            long customerId,
                            ProjectType type,
                            ArtType artType,
-                           ArtStyle artStyle);
+                           ArtStyle artStyle,
+                           PaymentType paymentType);
     boolean createProject (long userId,
                            String projectName,
                            Date deadline,
                            long customerId,
                            ProjectType type,
                            ToyType toyType,
-                           ToyStyle toyStyle);
+                           ToyStyle toyStyle,
+                           PaymentType paymentType);
 
     boolean editProject (long userId, Project editedProject);
     boolean deleteProject (long userId, Project project);
 
-    boolean createFursuitPart (long userId, String name);
+    boolean createFursuitPart (long userId, long fursuitId, String name);
     boolean editFursuitPart (long userId, FursuitPart editedFursuitPart);
-    boolean deleteFursuitPart (long userId, long partId);
+    boolean deleteFursuitPart (long userId, long projectId, long partId);
 
     Optional<FursuitPart> getFursuitPart (long userId, long id);
     List<FursuitPart> getFursuitPart (long userId);
-    //можно объединить
-    boolean addOutgoing (long userId, long projectId, long materialId, double amount);
-    //Пусть так будет
-    //Привяжем парт к конкретному проекту
-    boolean addOutgoing (long userId, long projectId, long fursuitPartId, long materialId, double amount);
-    boolean editOutgoing (long userId, Project project, Material material, double amount);
-    boolean deleteOutgoing (long userId, Project project, Material outgoing);
-
-    String getProjectEstimate (long userId);
+    boolean setOutgoing (long userId, long toyId, long materialId, double amount);
+    boolean setOutgoing (long userId, long projectId, long fursuitPartId, long materialId, double amount);
+    boolean deleteOutgoing(long userId, long toyId, long materialId);
+    boolean deleteOutgoing(long userId, long fursuitId, long fursuitPartId, long materialId);
+    String getProjectEstimate(long userId);
     String getProjectEstimate (long userId, long projectId);
 
     Optional <Project> getProject (long userId, long projectId);
@@ -72,7 +70,7 @@ public interface DataProvider {
 
     Double calculateProjectCost (Map <Material, Double> outgoingMap);
 
-    boolean createCustomer (long userId, String name, String url, String phoneNumber);
+    boolean createCustomer (long userId, String customerName, String url, String phoneNumber);
     boolean editCustomer (long userId, Customer editCustomer);
     boolean deleteCustomer (long userId, long customerId);
 
